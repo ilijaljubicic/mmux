d8c5633173d5c48f5076a78a383c81e2e57b795e
Ilija Ljubicic <iliya.ljubicic@gmail.com>
2026-06-01 01:11:16 +0300

    Use https git fecth mode.

--- example-backends/microsandbox/mmux_sources/scripts/25_install_mmux.sh
+++ example-backends/microsandbox/mmux_sources/scripts/25_install_mmux.sh
@@ -15,5 +15,24 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 export PATH="/root/.cargo/bin:/root/.local/bin:/usr/local/bin:$PATH"
 
-cargo install --locked --force --git "$MMUX_SOURCE_REPO" --rev "$MMUX_SOURCE_REF" --bin mmux --root /usr/local mmux
+case "$MMUX_SOURCE_REF" in
+  refs/heads/*)
+    ref_name="${MMUX_SOURCE_REF#refs/heads/}"
+    cargo install --locked --force --git "$MMUX_SOURCE_REPO" --branch "$ref_name" --bin mmux --root /usr/local mmux
+    ;;
+  refs/tags/*)
+    ref_name="${MMUX_SOURCE_REF#refs/tags/}"
+    cargo install --locked --force --git "$MMUX_SOURCE_REPO" --tag "$ref_name" --bin mmux --root /usr/local mmux
+    ;;
+  v[0-9]*)
+    cargo install --locked --force --git "$MMUX_SOURCE_REPO" --tag "$MMUX_SOURCE_REF" --bin mmux --root /usr/local mmux
+    ;;
+  *)
+    if printf '%s\n' "$MMUX_SOURCE_REF" | grep -Eq '^[0-9a-fA-F]{7,40}$'; then
+      cargo install --locked --force --git "$MMUX_SOURCE_REPO" --rev "$MMUX_SOURCE_REF" --bin mmux --root /usr/local mmux
+    else
+      cargo install --locked --force --git "$MMUX_SOURCE_REPO" --branch "$MMUX_SOURCE_REF" --bin mmux --root /usr/local mmux
+    fi
+    ;;
+esac
 printf '%s\n' "$desired_stamp" >"$stamp_file"
